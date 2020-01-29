c186d32834af903bbdf2b07fd5a1adbbfbbb68b9
Daniel B <1996.daniel.bryant@gmail.com>
2020-01-29 15:27:15 +0100

    added weight class to bmi

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -7,6 +7,9 @@ describe('User can check their Bmi', () => {
         cy.get('#Calculate').click()
     })
     it('displays a BMI value of 26.01', () => {
-		cy.get('#results').should('contain', '26.01')
-	})
+      cy.get('#results').should('contain', '26.01')
+    })
+    it('displays a message of you are Overweight', () => {
+      cy.get('#results').should('contain', 'Overweight')
+    })
 })
